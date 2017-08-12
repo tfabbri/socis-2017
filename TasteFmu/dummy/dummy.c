e0a8963eb4165b4ef6eaaa63de816a055521118a
Tommaso Fabbri <atfabbri@gmail.com>
2017-08-13 00:49:06 +0200

    DEV: added TasteFMU as starting point

---
+++ TasteFmu/dummy/dummy.c
@@ -0,0 +1,20 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "dummy.h"
+
+void dummy_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+}
+
+void dummy_PI_DummyFunction(const asn1SccMyReal *IN_mrp,
+                            const asn1SccMyInteger *IN_mip,
+                            const asn1SccMyBool *IN_mbp,
+                            asn1SccMyInteger *OUT_cip,
+                            asn1SccMyBool *OUT_cbp,
+                            asn1SccMyReal *OUT_crp)
+{
+    /* Write your code here! */
+}
+
---
+++ TasteFmu/dummy/dummy.h
@@ -0,0 +1,28 @@
+/* This file was generated automatically: DO NOT MODIFY IT ! */
+
+/* Declaration of the functions that have to be provided by the user */
+
+#ifndef __USER_CODE_H_dummy__
+#define __USER_CODE_H_dummy__
+
+#include "C_ASN1_Types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void dummy_startup();
+
+void dummy_PI_DummyFunction(const asn1SccMyReal *,
+                            const asn1SccMyInteger *,
+                            const asn1SccMyBool *,
+                            asn1SccMyInteger *,
+                            asn1SccMyBool *,
+                            asn1SccMyReal *);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
